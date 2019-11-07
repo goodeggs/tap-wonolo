122c7cebb09a5533d6aa800a6d826ec4c64f24a7
David Wallace <david.wallace@goodeggs.com>
2019-11-07 14:39:26 -0800

    init tap

--- tap_wonolo/__init__.py
+++ tap_wonolo/__init__.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 
@@ -5,8 +6,13 @@ import rollbar
 import singer
 from rollbar.logger import RollbarHandler
 
-from .client import WonoloStream
+from .client import JobRequestsStream, JobsStream, UsersStream
 
+AVAILABLE_STREAMS = {
+    JobsStream,
+    JobRequestsStream,
+    UsersStream
+}
 
 ROLLBAR_ACCESS_TOKEN = os.environ["ROLLBAR_ACCESS_TOKEN"]
 ROLLBAR_ENVIRONMENT = os.environ["ROLLBAR_ENVIRONMENT"]
@@ -24,21 +30,57 @@ REQUIRED_CONFIG_KEYS = [
     "environment"
 ]
 
-def discover():
-    pass
+
+def discover(args, select_all=False):
+    LOGGER.info('Starting discovery..')
+
+    catalog = {"streams": []}
+    for available_stream in AVAILABLE_STREAMS:
+        stream = available_stream.from_args(args)
+        catalog_entry = {
+            "stream": stream.tap_stream_id,
+            "tap_stream_id": stream.tap_stream_id,
+            "schema": stream.schema,
+            "metadata": singer.metadata.get_standard_metadata(schema=stream.schema,
+                                                              key_properties=stream.key_properties,
+                                                              valid_replication_keys=stream.bookmark_properties,
+                                                              replication_method=stream.replication_method)
+        }
+
+        if select_all is True:
+            catalog_entry["metadata"][0]["metadata"]["selected"] = True
+        catalog["streams"].append(catalog_entry)
+
+    print(json.dumps(catalog, indent=2))
+
 
 def sync(args):
-    wonolo = WonoloStream.from_args(args)
-    print(wonolo)
-    response = wonolo._generate_auth_token()
-    print(response)
+    LOGGER.info('Starting sync..')
+
+    if not args.state:
+        args.state = {"bookmarks": {}}
+
+    selected_streams = {catalog_entry.stream for catalog_entry in args.catalog.get_selected_streams(args.state)}
+
+    for available_stream in AVAILABLE_STREAMS:
+        if available_stream.tap_stream_id in selected_streams:
+            stream = available_stream.from_args(args)
+            LOGGER.info(f"Starting sync for stream {stream.tap_stream_id}..")
+            singer.bookmarks.set_currently_syncing(state=args.state, tap_stream_id=stream.tap_stream_id)
+            stream.write_state_message()
+            stream.write_schema_message()
+            stream.sync()
+            singer.bookmarks.set_currently_syncing(state=args.state, tap_stream_id=None)
+            stream.write_state_message()
+
 
 def main():
     args = singer.parse_args(required_config_keys=REQUIRED_CONFIG_KEYS)
     if args.discover:
-        discover()
+        discover(args, select_all=True)
     else:
         sync(args)
+
 
 if __name__ == "__main__":
     main()
