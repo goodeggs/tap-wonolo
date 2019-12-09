906ea289127fd056910c4adb7e7fe54f6da92035
David Wallace <david.wallace@goodeggs.com>
2019-12-09 11:23:55 -0500

    add tests for status code

--- tap_wonolo/client.py
+++ tap_wonolo/client.py
@@ -100,6 +100,7 @@ class WonoloStream(object):
         url = self.base_url + endpoint
         headers = self._construct_headers()
         response = requests.get(url, headers=headers, params=params)
+        print(response)
         response.raise_for_status()
         return response.json()
 
