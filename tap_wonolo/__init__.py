1d3298099a51ee8bbc2b5e050456d1616211c60c
David Wallace <david.wallace@goodeggs.com>
2020-01-13 16:22:15 -0500

    suggestions

--- tap_wonolo/__init__.py
+++ tap_wonolo/__init__.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 import rollbar
 import singer
@@ -74,7 +75,7 @@ def sync(args):
             stream.write_state_message()
 
 
-def main_impl():
+def _main():
     args = singer.parse_args(required_config_keys=REQUIRED_CONFIG_KEYS)
     if args.discover:
         discover(args, select_all=True)
@@ -84,13 +85,13 @@ def main_impl():
 
 def main():
     try:
-        main_impl()
-    except Exception as exc:
+        _main()
+    except Exception:
         if log_to_rollbar is True:
             LOGGER.info("Reporting exception info to Rollbar..")
             rollbar.report_exc_info()
-        LOGGER.critical(exc)
-        raise
+        LOGGER.critical(msg="Uncaught Exception..", exc_info=True)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
